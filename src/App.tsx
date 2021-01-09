cb187a13d3c4cb2520d1609f1211f3976b0d957b
Liam Percy <liam.percy@roguetwo.tech>
2021-01-09 19:49:21 +1000

    Cleaning and setting up firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "firebase";
+import { initialiseApp } from "./FirebaseConfig";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+initialiseApp();
 
-export default App;
+const db = firebase.firestore();
+const users = db.collection("users");
+
+export const App = () => {
+    users
+        .doc("liampercy123@gmail.com")
+        .get()
+        .then((d) => console.log(d.data()));
+    return <div>test</div>;
+};
